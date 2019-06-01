78ec6c8dd5580f0fbef0257f7e2ab0979730c2a5
Djordje Arsenovic <dj.arsenovic@gmail.com>
2019-06-01 13:36:40 +0200

    Added normalize.css to the application. Changed webpack config to accept scss and css files

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // Master Component
 import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
 import './styles/style.scss';
 // Render
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
