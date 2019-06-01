98cc88c2716086965ca005cd5f58eebb81509ab4
Djordje Arsenovic <dj.arsenovic@gmail.com>
2019-06-01 15:47:32 +0200

    Syled ReactModal component, fixed word-breaking problem.

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React from "react";
+import Modal from "react-modal";
 
-const OptionModal = (props) => (
+const OptionModal = props => (
   <Modal
     isOpen={!!props.selectedOption}
-    contentLabel='Selected Option'
+    contentLabel="Selected Option"
     onRequestClose={props.clearSelectedOption}
     ariaHideApp={false}
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected option:</h3>
-    <p>{props.selectedOption}</p>
-    <button onClick={props.clearSelectedOption}>Okay</button>
+    <h3 className="modal__title">Selected option:</h3>
+    <p className="modal__body">{props.selectedOption}</p>
+    <button className="button" onClick={props.clearSelectedOption}>
+      Okay
+    </button>
   </Modal>
 );
 
